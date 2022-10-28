0817eb619c524a387e98e93d7efc736ea4aefbbc
严欣他爹 <89913677+Alex-sunlight@users.noreply.github.com>
2022-10-28 18:03:20 +0800

    background6

--- src/pages/h5.vue
+++ src/pages/h5.vue
@@ -61,6 +61,175 @@
         </div>
       </div>
     </div>
+    <div class="background2_Box flex">
+      <img src="/h5/background2.png" class="background2" />
+      <div class="background2_content">
+        <div>
+          <div class="background2_size1">Non-custodial & Seamless User</div>
+          <div class="background2_size1">Onboarding Solution for Web2</div>
+          <div class="background2_size2">The best way to manage users' keys</div>
+        </div>
+        <div class="bt threeBox">
+          <div class="threeBox1 cl">
+            <img src="/h5/experience.png" class="threeBoxIm" />
+            <div class="threeBox_C">
+              <div>Seedless On-boarding</div>
+              <div>User Experience</div>
+            </div>
+            <div class="threeBox_B">
+              <div>Gain peace of mind without using</div>
+              <div>seed phrases, solving crypto</div>
+              <div>adoption key challenges.</div>
+            </div>
+          </div>
+          <div class="threeBox1 cl">
+            <img src="/h5/support.png" class="threeBoxIm" />
+            <div class="threeBox_C">
+              <div>Gasless & Multi-Chain</div>
+              <div>Support</div>
+            </div>
+            <div class="threeBox_B">
+              <div>Pay the gas with any token in any</div>
+              <div>chain. Compatible with all fastest-</div>
+              <div>growing EVM+ blockchains and</div>
+              <div>dApps.</div>
+            </div>
+          </div>
+          <div class="threeBox1 cl">
+            <img src="/h5/recovery.png" class="threeBoxIm" />
+            <div class="threeBox_C">
+              <div>Non-custodial & Social</div>
+              <div>Recovery</div>
+            </div>
+            <div class="threeBox_B">
+              <div>Take full control of wallet through</div>
+              <div>on-chain email social recovery. A</div>
+              <div>truly non-custodial, ERC-4337</div>
+              <div>compatible contract wallet.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background3_Box flex">
+      <div class="bt">
+        <div class="cl">
+          <div class="k">51K+</div>
+          <div class="gu">Global Users</div>
+        </div>
+        <div class="cl">
+          <div class="k">41</div>
+          <div class="gu">Integrated dApps</div>
+        </div>
+        <div class="background3BJ_Box flex">
+          <img src="/h5/background3BJ.png" class="background3BJ" />
+          <div class="background3BJ_S">
+            <div class="uIs">UniPass is</div>
+            <div class="powering">Powering</div>
+            <div class="uIs">Web3 Growth</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background4_Box cl">
+      <img src="/h5/background4Im1.png" class="background4Im1" />
+      <img src="/h5/background4Im2.png" class="background4Im2" />
+      <div style="margin-top: 182px">
+        <div class="Web3_S">Experience the Best Web3</div>
+        <div class="wallet_S">Smart Contract Wallet</div>
+      </div>
+      <!-- 线条 -->
+      <img src="/h5/line.png" class="line" />
+      <img src="/h5/point.png" class="point1" />
+      <img src="/h5/point.png" class="point2" />
+      <img src="/h5/point.png" class="point3" />
+      <div style="margin-left: 40px">
+        <div class="bt background4_center1">
+          <div style="margin-top: -220px">
+            <div class="background4_s1">Seedless Onboarding</div>
+            <div class="background4_s2">
+              <div>轻松通过邮件、Google、Twitter 及 Facebook 等人们</div>
+              <div>熟悉的账户体系登录 Web3 应用。大幅降低用户理解</div>
+              <div>和使用 dApps 的门槛。</div>
+            </div>
+          </div>
+          <div>
+            <img src="/h5/gif1.gif" />
+          </div>
+        </div>
+        <div class="background4_center2">
+          <div class="background4_s1">Gasless Experience</div>
+          <div class="background4_s2">
+            <div>
+              可以使用任意代币支付 gas，让用户无需额外理解 gas fee；或者可以通过 gas
+              赞助模式，对用户完全屏蔽 gas 概念。不再让
+            </div>
+            <div>用户在 dApps 使用中因为 gas 而感到困扰以致中断使用。</div>
+          </div>
+          <img src="/h5/gasless.svg" class="gasless_svg" />
+        </div>
+        <div class="bt background4_center3">
+          <div style="margin-top: -180px">
+            <div class="background4_s1">On-off Ramp Support</div>
+            <div class="background4_s2">
+              <div>通过接入多家出入金平台，UniPass 可以提供一站式的</div>
+              <div>加密资产购买和出售的体验，避免用户使用 dApp 过程</div>
+              <div>中需要切换多个 App 及网站的反人性体验。</div>
+            </div>
+            <div class="iconBox">
+              <img src="/H5/Ramp.svg" class="background4_Icon" />
+              <img src="/H5/Moonpay.svg" class="background4_Icon" />
+              <img src="/H5/BinanceConnect.svg" class="background4_Icon" />
+              <img src="/H5/WhaleFin.svg" class="background4_Icon" />
+              <img src="/H5/Wyre.svg" class="background4_Icon" />
+            </div>
+          </div>
+          <img src="/h5/buyUsdt.gif" class="buyUsdt" />
+        </div>
+      </div>
+    </div>
+    <div class="background5_Box flex">
+      <div class="bt background5">
+        <img src="/h5/background5.gif" class="background5_gif" />
+        <img src="/h5/line2.png" class="line2" />
+        <div>
+          <div class="background5_s1">
+            <div>Email-based Social</div>
+            <div>Recovery</div>
+          </div>
+          <div class="background5_s2">
+            <div>用户永远都有权通过自己的邮箱获取钱包的完整控制</div>
+            <div>权。并且可以通过设置更多的守护者，确保在自己的</div>
+            <div>邮箱失去控制时，同样能通过社交恢复方式找回钱包</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background6_Box flex"></div>
+    <div class="background7_Box flex">
+      <div class="background7 cl">
+        <div class="background7_topS">
+          <span>Wallet-</span>
+          <span class="background7_tops">as-a</span>
+          <span>-Service</span>
+        </div>
+        <div class="imgBox bt">
+          <img src="/h5/null.png" class="background7_img" />
+          <div>
+            <div>
+              <div class="background7_size">Powerful For Multi-</div>
+              <div class="background7_size">platform</div>
+            </div>
+            <div>
+              <el-button></el-button>
+              <el-button></el-button>
+              <el-button></el-button>
+              <el-button></el-button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -68,8 +237,279 @@
 useHead({ title: computed(() => $t('Notes')) })
 </script>
 <style lang="scss">
+// background7
+.background7_Box {
+  width: 100%;
+  height: 100%;
+  background-color: #18181e;
+}
+.background7 {
+  width: 100%;
+  margin-top: 300px;
+}
+.background7_topS {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 76px;
+  text-align: center;
+  color: #ffffff;
+}
+.background7_tops {
+  color: #ad94ff;
+}
+.background7_size {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 35px;
+  line-height: 60px;
+  text-align: right;
+  color: #ffffff;
+}
+.imgBox {
+  max-width: 1200px;
+  margin-top: 100px;
+}
+.background7_img {
+  width: 580px;
+  height: 360px;
+}
+// background7
+// background6
+.background6_Box {
+}
+// background6
+// background5
+.background5_Box {
+  width: 100%;
+  height: 100%;
+  background-color: #18181e;
+}
+.background5 {
+  margin-top: 180px;
+}
+.background5_gif {
+  width: 500px;
+  height: 520px;
+  margin-left: -50px;
+}
+.line2 {
+  width: 1px;
+  height: 640px;
+}
+.background5_s1 {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 60px;
+  text-align: right;
+  color: #ffffff;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+.background5_s2 {
+  margin-top: 40px;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 25px;
+  text-align: right;
+  color: rgba(255, 255, 255, 0.8);
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+}
+// background5
+// background4
+.background4_Box {
+  width: 100%;
+  height: 100%;
+  background-color: #18181e;
+  position: relative;
+  > * {
+    z-index: 10;
+  }
+}
+.background4Im1 {
+  width: 318px;
+  height: 628px;
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  top: 20px;
+}
+.background4Im2 {
+  max-width: 1000px;
+  max-height: 338.06px;
+  position: absolute;
+  z-index: 0;
+  top: 1530px;
+}
+.Web3_S {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 76px;
+  text-align: center;
+  color: #ffffff;
+}
+.wallet_S {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 76px;
+  text-align: center;
+  color: #ad94ff;
+}
+.line {
+  height: 2216px;
+  width: 2px;
+  position: absolute;
+  z-index: 20;
+  left: 13%;
+  top: 450px;
+}
+.point1 {
+  width: 24px;
+  height: 24px;
+  left: 12.3%;
+  top: 557px;
+  position: absolute;
+  z-index: 20;
+}
+.point2 {
+  width: 24px;
+  height: 24px;
+  top: 1257px;
+  left: 12.3%;
+  position: absolute;
+  z-index: 20;
+}
+.point3 {
+  width: 24px;
+  height: 24px;
+  top: 2260px;
+  left: 12.3%;
+  position: absolute;
+  z-index: 20;
+}
+.background4_s1 {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 35px;
+  line-height: 60px;
+  color: #ffffff;
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+}
+.background4_s2 {
+  margin-top: 40px;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 25px;
+  color: rgba(255, 255, 255, 0.8);
+}
+.background4_center1 {
+  margin-top: 140px;
+  margin-left: 20px;
+}
+.background4_center2 {
+  margin-top: 240px;
+  margin-left: 20px;
+}
+.gasless_svg {
+  margin-top: 120px;
+}
+.background4_center3 {
+  margin-top: 240px;
+  margin-left: 20px;
+}
+.buyUsdt {
+  width: 500px;
+  height: 520px;
+  margin-left: 80px;
+}
+.background4_Icon {
+  width: 40px;
+  height: 40px;
+  margin-left: 25px;
+}
+.iconBox {
+  margin: 30px 0 0 -30px;
+}
+// background4
+// background3
+.background3_Box {
+  width: 100%;
+  height: 100%;
+  background-color: #18181e;
+}
+.background3BJ {
+  width: 464px;
+  height: 464px;
+}
+.background3BJ_Box {
+  position: relative;
+  > * {
+    z-index: 0;
+  }
+}
+.background3BJ_S {
+  position: absolute;
+  z-index: 10;
+}
+.k {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 74px;
+  line-height: 74px;
+  text-align: right;
+  color: #ffffff;
+}
+.gu {
+  margin-top: 26px;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 40px;
+  color: rgba(255, 255, 255, 0.595813);
+}
+.uIs {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 52px;
+  line-height: 74px;
+  text-align: right;
+  color: #ffffff;
+}
+.powering {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 62px;
+  line-height: 74px;
+  text-align: right;
+  color: #ad94ff;
+}
+// background3
+
 // background1
 .background1_Box {
+  width: 100%;
   position: relative;
   > * {
     z-index: 0;
@@ -93,7 +533,7 @@ useHead({ title: computed(() => $t('Notes')) })
 }
 .background1Top_content_ {
   width: 100%;
-  max-width: 1200px;
+  max-width: 1000px;
   padding: 0 10px;
   top: 40px;
   z-index: 10;
@@ -243,4 +683,93 @@ useHead({ title: computed(() => $t('Notes')) })
   right: 230px;
 }
 // background1
+// background2
+.background2_Box {
+  width: 100%;
+  position: relative;
+  > * {
+    z-index: 0;
+  }
+  background-color: #18181e;
+}
+.background2 {
+  width: 1683.86px;
+  height: 894.62px;
+}
+.background2_content {
+  width: 100%;
+  max-width: 1000px;
+  position: absolute;
+  z-index: 10;
+}
+.background2_size1 {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 44px;
+  line-height: 66px;
+  text-align: center;
+  color: #ffffff;
+}
+.background2_size2 {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 40px;
+  text-align: center;
+  background: linear-gradient(180deg, #b18cff 27.5%, #8864ff 108.75%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-fill-color: transparent;
+}
+.bt {
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.threeBox {
+  margin-top: 100px;
+}
+.cl {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.threeBox1 {
+}
+.threeBoxIm {
+  width: 120px;
+  height: 120px;
+}
+.threeBox_C {
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 36px;
+  text-align: center;
+  color: #ffffff;
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+}
+.threeBox_B {
+  margin-top: 24px;
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.8);
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+}
+// background2
 </style>
